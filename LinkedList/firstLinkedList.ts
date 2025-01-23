c55a8bafd1cc1ea20f52a3e64208727fdca94396
aklesh-dev <arzunayavadu998@gmail.com>
2025-01-23 09:02:17 +0545

    Linked List - create push method.

---
+++ LinkedList/firstLinkedList.ts
@@ -0,0 +1,39 @@
+// --Node
+class LinkedListNode<T> {
+  head: T;
+  next: LinkedListNode<T> | null;
+  constructor(value: T) {
+    this.head = value;
+    this.next = null;
+  }
+}
+
+class LinkedList<T> {
+  head: LinkedListNode<T> | null;
+  tail: LinkedListNode<T> | null;
+  length: number;
+  constructor(value: T) {
+    this.head = new LinkedListNode(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  // push method. Adds a new node to the end of the list.
+  push(value: T) {
+    let newNode = new LinkedListNode<T>(value);
+    // if list is empty
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // add to last node
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+}
+
+const myLinkedList = new LinkedList(1);
+myLinkedList.push(12);
+console.log(myLinkedList);
---
+++ LinkedList/linkedList.md
@@ -0,0 +1,22 @@
+1. Single Linked List.
+- Push(Challenge)
+- Pop(Challenge)
+- Unshift(Challenge)
+- Shift(Challenge)
+- GetFirst(Challenge)
+- GetLast(Challenge)
+- Set(Challenge)
+- Inset(Challenge)
+- Size(Challenge)
+- Clear(Challenge)
+
+2. Double LinkedList.
+- Push(Challenge)
+- Pop(Challenge)
+- Unshift(Challenge)
+- Shift(Challenge)
+
+3. Reverse LinkedList.
+
+## Linked List
+### A listed list is a linear datat structure where elements, called nodes, are stored contigously in memory. Instead, each node contains data and a reference, or link, to the next node in the sequence. The first node is called the head and the last node is called the tail.
