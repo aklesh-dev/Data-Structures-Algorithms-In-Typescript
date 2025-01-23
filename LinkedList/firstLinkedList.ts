d9ce2acf84292c3d873f3c7f0285f623b0c555f6
aklesh-dev <arzunayavadu998@gmail.com>
2025-01-23 09:48:07 +0545

    pop method.

--- LinkedList/firstLinkedList.ts
+++ LinkedList/firstLinkedList.ts
@@ -31,9 +31,42 @@ class LinkedList<T> {
       this.tail = newNode;
     }
     this.length++;
-  }
-}
+  };
+
+
+  // pop method. Removes and returns the last node of the list.
+  pop():T  {
+
+    // --if list is empty
+    if (!this.head) {
+      return null;
+    };
+
+    let temp = this.head;
+    let prev = this.head;
+
+    while (temp.next) {
+      prev = temp;  // --> update prev node
+      temp = prev.next; // --> update temp node
+    }
+
+    this.tail = prev; // update tail node
+    this.tail.next = null; // remove link to next node
+    this.length--;
+
+    // --if list has only one node
+    if(this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    };
+
+    return temp.head; // return the removed node
+  };
+    
+  
+};
 
 const myLinkedList = new LinkedList(1);
 myLinkedList.push(12);
+myLinkedList.pop();
 console.log(myLinkedList);
